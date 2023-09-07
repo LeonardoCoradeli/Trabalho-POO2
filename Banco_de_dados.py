59fc74c2f9d8c71ec96c625118e1981798034fd9
Marco Vinicius de Melo Faria <marco.melo-faria@unesp.br>
2023-09-07 14:18:23 -0300

    Alguns erros de digitação arrumados em Veiculos.py e desenvolvimento do Banco_de_dados.py

--- Banco_de_dados.py
+++ Banco_de_dados.py
@@ -13,9 +13,49 @@ class BancodeDados:
     @staticmethod
     def criarFuncionario(funcionario):
         funcionario_dic = funcionario.__dict__
-        response = requests.put(f"{BancodeDados.URLBanco}{BancodeDados.URLTFuncionarios}/{funcionario_dic['_codigoUsuario']}.json",
-                                data=json.dumps(funcionario_dic))
+        response = requests.put(f"{BancodeDados.URLBanco}{BancodeDados.URLTFuncionarios}/{funcionario_dic['_codigoUsuario']}.json",data=json.dumps(funcionario_dic))
         if response.status_code == 200:
             print("Enviado com sucesso!\n")
         else:
             print("Não enviado!\n")
+
+    @staticmethod
+    def recuperarFuncionario(codigoUsuario):
+        response = requests.get(f"{BancodeDados.URLBanco}{BancodeDados.URLTFuncionarios}/{codigoUsuario}.json")
+
+        if response.status_code == 200:
+            data = response.json()
+            if data:
+                print(data)
+            else:
+                print("Funcionário não encontrado no banco de dados.")
+        else:
+            print("Erro ao recuperar informações do funcionário.")
+
+    @staticmethod
+    def criarLocacao(locacao):
+        locacao_dic = locacao.__dict__
+        dataLocacao = locacao_dic["_dataLocacao"].strftime("%d/%m/%Y")
+        dataDevolucao = locacao_dic["_dataDevolucao"].strftime("%d/%m/%Y")
+        locacao_dic["_dataLocacao"] = dataLocacao
+        locacao_dic["_dataDevolucao"] = dataDevolucao
+        response = requests.put(f"{BancodeDados.URLBanco}{BancodeDados.URLTabelaLocacoes}/{locacao_dic['_codLocacao']}.json",data=json.dumps(locacao_dic))
+        if response.status_code == 200:
+            print("Enviado com sucesso")
+        else:
+            print("Não foi possível enviar")
+
+
+    @staticmethod
+    def recuperarLocacao(codLocacao):
+        response = requests.get(f"{BancodeDados.URLBanco}{BancodeDados.URLTabelaLocacoes}/{codLocacao}.json")
+
+
+        if response.status_code == 200:
+            data = response.json()
+            if data:
+                print(data)
+            else:
+                print("Locacao não encontrada no banco de dados.")
+        else:
+            print("Erro ao recuperar informações da locacao.")
