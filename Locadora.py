e3b09f098e4df46bda353550a63f177039832322
Leonardo Cenedes Pereira <leonardo.cenedes@unesp.br>
2023-09-28 23:53:28 -0300

    Update Locadora.py

--- Locadora.py
+++ Locadora.py
@@ -16,5 +16,106 @@ class Locadora:
         self._seguros = self._conifguracao.recuperarTodosSeguros()
         self._veiculos = self._conifguracao.recuperarTodosVeiculosNacionais()+self._conifguracao.recuperarTodosVeiculosImportados()
 
+    def ListarVeiculos(self):
+        for i in self._veiculos:
+            print(i)
+    
+    def ListarVeiculosNacionais(self):
+        for i in self._veiculos:
+            if i.getTipo() == "Nacional":
+                print(i)
+    
+    def ListaVeiculosImportados(self):
+        for i in self._veiculos:
+            if i.getTipo() == "Importado":
+                print(i)
+    
+    def ListasVeiulosDisponiveis(self):
+        for i in self._veiculos:
+            if i.getDisponivel() == True:
+                print(i)
+    
+    def ListarVeiculosDisponiveisCategoria(self,categoria):
+        for i in self._veiculos:
+            if i.getDisponivel() == True and i.getCategoria() == categoria:
+                print(i)
+    
+    def ListarVeiculosNãoDisponiveis(self):
+        for i in self._veiculos:
+            if i.getDisponivel() == False:
+                print(i)
+    
+    def ListarVeiculosAtrasados(self):
+        for i in self._locacoes:
+            if i.getAtrasado() == True:
+                print(i)
+    
+    def ListarClientesComLocacao(self):
+        for i in self._locacoes:
+            print(i.getCliente())
+    
+    def ListarFuncionarios(self):
+        for i in self._funcionarios:
+            print(i)
+    
+    def ListarFuncionarioDoMes(self):
+        for i in self._funcionarios:
+            if i.getFuncionarioDoMes() == True:
+                print(i)
+    
+    def ListarClientes(self):
+        for i in self._clientes:
+            print(i)
+    
+    def ListarHistoricoLocacaoCliente(self,cliente):
+        for i in self._locacoes:
+            if i.getCliente() == cliente:
+                print(i)
+    
+    def ListarTodasLocacoes(self):
+        for i in self._locacoes:
+            print(i)
+    
+    def ListarLocacaoMes(self,mes):
+        for i in self._locacoes:
+            if i.getMes() == mes:
+                print(i)
+
+    def ListarLocacaoMescomLucro(self,mes):
+        for i in self._locacoes:
+            if i.getMes() == mes:
+                print(i.getLucro())
+    
+    def ListarLocacoesFinalizadas(self):
+        for i in self._locacoes:
+            if i.getFinalizada() == True:
+                print(i)
+    
+    def ListarLocacoesNaoFinalizadas(self):
+        for i in self._locacoes:
+            if i.getFinalizada() == False:
+                print(i)
+    
+    def ListarLocacoesNaoFinalizadasNacional(self):
+        for i in self._locacoes:
+            if i.getFinalizada() == False and i.getVeiculo().getTipo() == "Nacional":
+                print(i)
+    
+    def ListarLocacoesNaoFinalizadasImportado(self):
+        for i in self._locacoes:
+            if i.getFinalizada() == False and i.getVeiculo().getTipo() == "Importado":
+                print(i)
+
+    def ListarLocacoesAtrasadas(self):
+        for i in self._locacoes:
+            if i.getAtrasado() == True:
+                print(i)
+    
+    def ListarTiposSeguros(self):
+        for i in self._seguros:
+            print(i)
+    
+    def RetornarVeiculoscomoObjeto(self):
+        return self._veiculos
 
 
